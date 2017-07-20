76737324853ed59d3f5c1a10bf7cbfb6e905c6b5
Kevin Owens <kowens@gmail.com>
2017-07-19 23:09:41 -0600

    Added idle animation for player, plus our first enemy: a Cyber Death Ball

---
+++ Assets/Scripts/CyberDeathBall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CyberDeathBall : MonoBehaviour
+{
+    public float hoverForce = 1f;
+    public float devianceTolerance = 0.25f;
+    public float maximumVelocity = 1f;
+
+    private Animator animator;
+    private Rigidbody2D rigidBody;
+
+    private float originalY;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        originalY = rigidBody.transform.position.y;
+    }
+
+    void Update()
+    {
+        if (rigidBody.transform.position.y < originalY - devianceTolerance && rigidBody.velocity.magnitude < maximumVelocity)
+        {
+            rigidBody.AddForce(Vector2.up * hoverForce);
+        }
+    }
+}
