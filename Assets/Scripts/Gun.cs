a68401c4c19803213d34e696fe038e73df5d38fd
Kevin Owens <kowens@gmail.com>
2017-07-22 18:48:24 -0600

    Add a cooldown timer to the gun.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public float CoolDownPeriodInSeconds = 1f;
+    public float bulletForce = 5f;
+    public GameObject bullet;
+    public Transform bulletSpawn;
+
+	private float coolDownTimeStamp;
+
+	void Start () {	
+	}
+	
+	void Update () {
+	}
+
+	public bool Shoot(bool isFacingRight) {
+		if (coolDownTimeStamp <= Time.time) {
+			coolDownTimeStamp = Time.time + CoolDownPeriodInSeconds;
+
+			var bulletInstance = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+			var forceDirection = isFacingRight ? Vector2.right : Vector2.left;
+			var bulletRigidBody2d = bulletInstance.GetComponent<Rigidbody2D>();
+
+			if (!isFacingRight)
+			{
+				bulletInstance.transform.localScale = new Vector2(-bulletInstance.transform.localScale.x, bulletInstance.transform.localScale.y);
+			}
+
+			bulletInstance.GetComponent<Rigidbody2D>().AddForce(forceDirection * bulletForce);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,9 +4,7 @@ public class PlayerMovementController : MonoBehaviour
 {
     public float jumpForce = 150f;
     public float runningSpeed = 2.0f;
-    public float bulletForce = 300f;
-    public GameObject bullet;
-    public Transform bulletSpawn;
+    public Gun Gun;
 
     private bool grounded = true;
     private bool isFacingRight = true;
@@ -16,14 +14,12 @@ public class PlayerMovementController : MonoBehaviour
 
     void Start()
     {
-
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-
         var walkingInput = Input.GetAxisRaw("Horizontal");
         var jumpingInput = Input.GetAxisRaw("Vertical");
 
@@ -52,23 +48,14 @@ public class PlayerMovementController : MonoBehaviour
 
         if (Input.GetButton("Fire1") || Input.GetKeyDown("space"))
         {
-            animator.SetBool("IsShooting", true);
-
-            var bulletInstance = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-            var forceDirection = isFacingRight ? Vector2.right : Vector2.left;
-            var bulletRigidBody2d = bulletInstance.GetComponent<Rigidbody2D>();
-
-            if (!isFacingRight)
-            {
-                bulletInstance.transform.localScale = new Vector2(-bulletInstance.transform.localScale.x, bulletInstance.transform.localScale.y);
+            if (Gun.Shoot(isFacingRight)) {                
+                animator.SetBool("IsShooting", true);
             }
-
-            bulletInstance.GetComponent<Rigidbody2D>().AddForce(forceDirection * bulletForce);
         }
 
         RespawnIfPlayerHasFallenToHisDoom();
     }
-    
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         grounded = true;
