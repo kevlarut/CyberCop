4e60ef0de10e75a0f7a68703e2f39f1927208402
Kevin Owens <kowens@gmail.com>
2019-03-06 08:34:36 -0700

    Starting to get the Parallax backgrounds to wrap around.

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -3,21 +3,41 @@
  
  public class Parallax : MonoBehaviour {
      
+     public bool IsSecondCopy = false;
     public float Multiplier = 0.1f;
     public Transform Target;
 
-    private float x;
+    private float previousX;
 
     void Start() {
-        x = Target.position.x;
+        previousX = Target.position.x;
+
+        if (IsSecondCopy) {
+            var newX = transform.position.x + GetLength();
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
     }
 
     void Update () 
     {
-        if (Target.position.x != x) {
-            var newX = transform.position.x - (Target.position.x - x ) * Multiplier;
+        if (Target.position.x != previousX) {
+            var newX = transform.position.x - (Target.position.x - previousX) * Multiplier;
+
+            var wrapLength = GetLength() * (IsSecondCopy ? 2 : 1);
+            if (transform.position.x < Target.position.x - wrapLength) {
+                newX = Target.position.x + wrapLength;
+            }
+            else if (transform.position.x > Target.position.x + wrapLength) {
+                newX = Target.position.x - wrapLength;
+            }
+
             transform.position = new Vector3(newX, transform.position.y, transform.position.z);
-            x = Target.position.x;
+            previousX = Target.position.x;
         }
+    }
+
+    float GetLength() {
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        return spriteRenderer.bounds.size.x;
     }
  }
